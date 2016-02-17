8b75bd667a3d040dba3bcf4f89a0c8e628bfcc1a
tangzhi <tangzhi@asiainfo.com>
2016-02-17 08:57:12 +0800

    .

--- test/web.test.js
+++ test/web.test.js
@@ -11,18 +11,27 @@ describe('/\\.html/', function test() {
   it('index.html should be ok', function testOK(done) {
     request
       .get('/index.html')
-      .expect(200, content, done);
+      .expect(200, content)
+      .expect('ETag', /^W\//)
+      .expect('Last-Modified', /.*/)
+      .end(done);
   });
 
   it('login.html should be ok', function testOK(done) {
     request
       .get('/login.html')
-      .expect(200, content, done);
+      .expect(200, content)
+      .expect('ETag', /^W\//)
+      .expect('Last-Modified', /.*/)
+      .end(done);
   });
 
   it('other uri should be ok', function testOK(done) {
     request
       .get('/other')
-      .expect(200, content, done);
+      .expect(200, content)
+      .expect('ETag', /^W\//)
+      .expect('Last-Modified', /.*/)
+      .end(done);
   });
 });
