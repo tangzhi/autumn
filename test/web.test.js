8abaf18ac15b7e931c7c2a5c9d5feaf402c30da0
tangzhi <tangzhi@asiainfo.com>
2016-02-22 10:17:11 +0800

    add headers check

--- test/web.test.js
+++ test/web.test.js
@@ -1,5 +1,6 @@
 var app = require('../index');
 var request = require('supertest')(app);
+var should = require('should');
 var join = require('path').join;
 var read = require('fs').readFileSync;
 
@@ -7,6 +8,8 @@ var read = require('fs').readFileSync;
 describe('/\\.html/', function test() {
   var path = join(__dirname, '../web', 'index.html');
   var content = read(path, { encoding: 'utf8' });
+  var etag;
+  var lastmodified;
 
   it('index.html should be ok', function testOK(done) {
     request
@@ -14,6 +17,28 @@ describe('/\\.html/', function test() {
       .expect(200, content)
       .expect('ETag', /^W\//)
       .expect('Last-Modified', /.*/)
+      .expect(function get(res) {
+        should.exist(res.headers.etag);
+        should.exist(res.headers['last-modified']);
+        etag = res.headers.etag;
+        lastmodified = res.headers['last-modified'];
+      })
+      .end(done);
+  });
+
+  it('request index.html with If-Modified-Since should return 304', function test304(done) {
+    request
+      .get('/index.html')
+      .set('If-Modified-Since', lastmodified)
+      .expect(304, '')
+      .end(done);
+  });
+
+  it('request index.html with If-None-Match should return 304', function test304(done) {
+    request
+      .get('/index.html')
+      .set('If-None-Match', etag)
+      .expect(304, '')
       .end(done);
   });
 
